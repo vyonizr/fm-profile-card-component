910f37cc0b34c75d6a5dae32a371dca6e6350030
Fitrahtur Rahman <fitrahtur@gmail.com>
2023-05-05 08:55:37 +0700

    create mobile layout

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'kumbh': ['"Kumbh Sans"', 'ui-sans-serif', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
